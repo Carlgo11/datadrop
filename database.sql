d991bd8b8087b1696f343f1a2aba5d69cba8dd0b
Carlgo11 <github@carlgo11.com>
2016-07-01 20:46:59 +0200

    Update Mysql Database Structure

--- database.sql
+++ database.sql
@@ -1,7 +1,16 @@
 # MySQL Database
+CREATE DATABASE `datadrop` /*!40100 DEFAULT CHARACTER SET latin1 */
+
 CREATE TABLE `users` (
  `username` text NOT NULL,
  `password` text NOT NULL,
- `salt` text NOT NULL,
  `yubikey` text NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1
+
+CREATE TABLE `files` (
+ `id` int(11) NOT NULL AUTO_INCREMENT,
+ `author` text NOT NULL,
+ `metadata` mediumtext NOT NULL,
+ `content` mediumblob NOT NULL,
+ PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=29 DEFAULT CHARSET=latin1
